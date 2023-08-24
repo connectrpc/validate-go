a9178b0cfc79cb41f0a75ef0dae733c5a93cfa9a
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2023-08-24 15:31:12 -0400

    checkpoint

--- interceptor.go
+++ interceptor.go
@@ -5,32 +5,9 @@ import (
 	"errors"
 
 	"connectrpc.com/connect"
-
 	"github.com/bufbuild/protovalidate-go"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 )
-
-type Interceptor struct {
-	validator *protovalidate.Validator
-}
-
-func NewInterceptor(validator *protovalidate.Validator, _ ...Option) *Interceptor {
-	return &Interceptor{validator: validator}
-}
-
-func (i *Interceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
-	return NewUnaryFunc(i.validator)(next)
-}
-
-func (i *Interceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
-	return NewStreamingClientFunc(i.validator)(next)
-}
-
-func (i *Interceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
-	return NewStreamingHandlerFunc(i.validator)(next)
-}
 
 // Option interface is currently empty and serves as a placeholder for potential future implementations.
 // It allows adding new options without breaking existing code.
@@ -38,27 +15,50 @@ type Option interface {
 	unimplemented()
 }
 
-// NewUnaryFunc returns a new UnaryFunc that validates the request and response
-func NewUnaryFunc(validator *protovalidate.Validator, _ ...Option) connect.UnaryInterceptorFunc {
-	return func(next connect.UnaryFunc) connect.UnaryFunc {
-		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-			if err := validate(validator, req.Any()); err != nil {
-				return nil, err
-			}
-			return next(ctx, req)
+// Interceptor implements connect.Interceptor.
+type Interceptor struct {
+	validator *protovalidate.Validator
+	options   []Option
+}
+
+// NewInterceptor returns a new Interceptor.
+func NewInterceptor(
+	validator *protovalidate.Validator,
+	opts ...Option,
+) *Interceptor {
+	return &Interceptor{
+		validator: validator,
+		options:   opts,
+	}
+}
+
+// WrapUnary returns a new connect.UnaryFunc that wraps the given connect.UnaryFunc.
+func (i *Interceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
+	return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+		if err := validate(i.validator, req.Any()); err != nil {
+			return nil, err
+		}
+		return next(ctx, req)
+	}
+}
+
+// WrapStreamingClient returns a new connect.StreamingClientFunc that wraps the given connect.StreamingClientFunc.
+func (i *Interceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
+	return func(ctx context.Context, spec connect.Spec) connect.StreamingClientConn {
+		return &streamingClientInterceptor{
+			validator:           i.validator,
+			StreamingClientConn: next(ctx, spec),
 		}
 	}
 }
 
-// NewStreamingClientFunc returns a new StreamingClientFunc that validates the request and response
-func NewStreamingClientFunc(validator *protovalidate.Validator, _ ...Option) func(next connect.StreamingClientFunc) connect.StreamingClientFunc {
-	return func(next connect.StreamingClientFunc) connect.StreamingClientFunc {
-		return func(ctx context.Context, spec connect.Spec) connect.StreamingClientConn {
-			return &streamingClientInterceptor{
-				validator:           validator,
-				StreamingClientConn: next(ctx, spec),
-			}
-		}
+// WrapStreamingHandler returns a new connect.StreamingHandlerFunc that wraps the given connect.StreamingHandlerFunc.
+func (i *Interceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
+	return func(ctx context.Context, conn connect.StreamingHandlerConn) error {
+		return next(ctx, &streamingHandlerInterceptor{
+			validator:            i.validator,
+			StreamingHandlerConn: conn,
+		})
 	}
 }
 
@@ -67,20 +67,9 @@ type streamingClientInterceptor struct {
 	connect.StreamingClientConn
 }
 
+// Receive implements connect.StreamingClientConn.
 func (s *streamingClientInterceptor) Receive(msg any) error {
 	return validate(s.validator, msg)
-}
-
-// NewStreamingHandlerFunc returns a new StreamingHandlerFunc that validates the request and response
-func NewStreamingHandlerFunc(validator *protovalidate.Validator, _ ...Option) func(connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
-	return func(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
-		return func(ctx context.Context, conn connect.StreamingHandlerConn) error {
-			return next(ctx, &streamingHandlerInterceptor{
-				validator:            validator,
-				StreamingHandlerConn: conn,
-			})
-		}
-	}
 }
 
 type streamingHandlerInterceptor struct {
@@ -88,6 +77,7 @@ type streamingHandlerInterceptor struct {
 	connect.StreamingHandlerConn
 }
 
+// Receive implements connect.StreamingHandlerConn.
 func (s *streamingHandlerInterceptor) Receive(msg any) error {
 	return validate(s.validator, msg)
 }
@@ -98,7 +88,7 @@ func validate(validator *protovalidate.Validator, msg any) error {
 		return errors.New("unsupported message type")
 	}
 	if err := validator.Validate(message); err != nil {
-		return status.Error(codes.InvalidArgument, err.Error())
+		return connect.NewError(connect.CodeInvalidArgument, err)
 	}
 	return nil
 }
