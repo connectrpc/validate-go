89db5fe37801fefa499ace35233eed98de3f1604
Carlos Miguel Garcia <89794450+cmgsj@users.noreply.github.com>
2025-02-01 05:18:45 -0500

    Add support for protovalidate breaking changes (#29)
    Add support for protovalidate and protovalidate-go breaking changes.
    
    ---------
    
    Signed-off-by: cmgsj <cmgxcix@gmail.com>

--- go.mod
+++ go.mod
@@ -1,24 +1,25 @@
 module connectrpc.com/validate
 
-go 1.21.0
+go 1.21.1
 
 require (
-	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.35.1-20240920164238-5a7b106cbb87.1
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.4-20250130201111-63bb56e20495.1
 	connectrpc.com/connect v1.17.0
-	github.com/bufbuild/protovalidate-go v0.7.2
-	github.com/stretchr/testify v1.9.0
-	google.golang.org/protobuf v1.35.1
+	github.com/bufbuild/protovalidate-go v0.9.1
+	github.com/stretchr/testify v1.10.0
+	google.golang.org/protobuf v1.36.4
 )
 
 require (
+	cel.dev/expr v0.19.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/google/cel-go v0.21.0 // indirect
+	github.com/google/cel-go v0.23.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240401170217-c3f982113cda // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240826202546-f6391c0de4c7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240826202546-f6391c0de4c7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- validate_test.go
+++ validate_test.go
@@ -102,7 +102,7 @@ func TestInterceptorUnary(t *testing.T) {
 					violations, ok := detail.(*validatepb.Violations)
 					require.True(t, ok)
 					require.Len(t, violations.Violations, 1)
-					require.Equal(t, test.wantPath, violations.Violations[0].GetFieldPath())
+					require.Equal(t, test.wantPath, protovalidate.FieldPathString(violations.Violations[0].GetField()))
 				}
 			} else {
 				require.NoError(t, err)
@@ -188,7 +188,7 @@ func TestInterceptorStreamingHandler(t *testing.T) {
 					violations, ok := detail.(*validatepb.Violations)
 					require.True(t, ok)
 					require.Len(t, violations.Violations, 1)
-					require.Equal(t, test.wantPath, violations.Violations[0].GetFieldPath())
+					require.Equal(t, test.wantPath, protovalidate.FieldPathString(violations.Violations[0].GetField()))
 				}
 			} else {
 				require.NoError(t, err)
@@ -275,7 +275,7 @@ func TestInterceptorStreamingClient(t *testing.T) {
 					violations, ok := detail.(*validatepb.Violations)
 					require.True(t, ok)
 					require.Len(t, violations.Violations, 1)
-					require.Equal(t, test.wantPath, violations.Violations[0].GetFieldPath())
+					require.Equal(t, test.wantPath, protovalidate.FieldPathString(violations.Violations[0].GetField()))
 				}
 			} else {
 				require.NoError(t, err)
@@ -293,7 +293,7 @@ func TestInterceptorStreamingClient(t *testing.T) {
 
 func TestWithValidator(t *testing.T) {
 	t.Parallel()
-	validator, err := protovalidate.New(protovalidate.WithDisableLazy(true))
+	validator, err := protovalidate.New(protovalidate.WithDisableLazy())
 	require.NoError(t, err)
 	interceptor, err := validate.NewInterceptor(validate.WithValidator(validator))
 	require.NoError(t, err)
